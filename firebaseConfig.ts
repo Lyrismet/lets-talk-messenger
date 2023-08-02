91fede4dd7d9f330b8a3003405b7557abf1afeba
lyrismet <ana0203tir@gmail.com>
2023-08-02 16:23:27 +0300

    feat: added config file for firebase

---
+++ firebaseConfig.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAWTLdWjPXP0nqdWkDOa17iROKbDIO8zVU",
+    authDomain: "let-s-talk-social-app.firebaseapp.com",
+    projectId: "let-s-talk-social-app",
+    storageBucket: "let-s-talk-social-app.appspot.com",
+    messagingSenderId: "755145875089",
+    appId: "1:755145875089:web:d5254a6db218df55a1a930",
+    measurementId: "G-NR2PP2ZC3R"
+};
