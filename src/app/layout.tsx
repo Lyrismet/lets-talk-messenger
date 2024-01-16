1002fb231f2fd134319c8a9e2f21a68f45a8f9d5
Lyrismet <ana0203tir@gmail.com>
2024-01-16 14:57:17 +0300

    feat: added react import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Poppins } from 'next/font/google'
+import React from "react";
 
 const poppins = Poppins({weight: ['400', '600', '700'], subsets: ['latin'] })
 
