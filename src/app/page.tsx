82788b22e9b46fbc24bbae6adb9ebe7974b81ae4
Lyrismet <ana0203tir@gmail.com>
2024-01-16 20:48:51 +0300

    ref: optimized exports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,10 @@ import {useAuthState} from "react-firebase-hooks/auth";
 
 import ChatRoom from "@/components/ChatRoom/ChatRoom";
 import SignIn from "@/components/SignIn/SignIn";
-import {app, config, firestore} from "@/app/firebaseExports";
+import {auth} from "@/app/firebaseExports";
 
 export default function Home() {
-    const [user] = useAuthState(config);
+    const [user] = useAuthState(auth);
 
     return (
         <main className={styles.main}>
